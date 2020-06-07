760d14e1334a42bd109cd55b82ce7f3c604f8904
Nick Hart <nick.hart.des@gmail.com>
2020-06-06 19:37:50 -0500

    Add playCard and showTopCard functions, refactor CSS to include styling for center card image

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener('keydown', playerActions);
 
 function playerActions(event) {
   if (event.key === 'q' && test.turnCounter % 2 === 0) {
-    test.playCard(test.player1);
+    playCard(test.player1);
   } else if (event.key === 'f') {
     test.slap(test.player1);
   } else if (event.key === 'p' && test.turnCounter % 2 != 0) {
@@ -17,4 +17,17 @@ function playerActions(event) {
 
 function gameSetup() {
   test.dealCards();
+}
+
+function playCard(player) {
+  test.playCard(player);
+  showTopCard();
+}
+
+function showTopCard() {
+  var centerPileContainer = document.querySelector('.card-pile');
+  var cardPileImage = document.querySelector('.card-pile-image');
+
+  centerPileContainer.classList.remove('hidden');
+  cardPileImage.src = test.cardPile[0];
 }
